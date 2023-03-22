d271f8f30d484ee7ca8e7958ac1ca4ab6e252490
kurniadikevin <kurniadikevin@gmail.com>
2023-03-22 18:04:46 +0700

    notification toggle for like and comment

--- src/pages/Notification/index.js
+++ src/pages/Notification/index.js
@@ -4,7 +4,7 @@ import Dashboard from '../../components/dashboard/dashboard';
 import Sidebar from '../../components/sidebar/sidebar';
 import axios from 'axios';
 import { toggleLoader } from '../../components/loader/loader-toggle';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { refreshLoginSession } from '../../components/functions';
 
 export function NotificationPage() {
@@ -58,16 +58,39 @@ export function NotificationPage() {
         },
         url : `http://localhost:5000/users/friendRequest/decline/${currentUser._id}`
     }).then(function (response) {
-    /*   const alertBox = document.querySelector('#alert-box');
-      alertBox.style.display='inline';
-      alertBox.textContent='Decline friend request';
-      const requestContainer = document.querySelectorAll('.friendReq-cont');
-      requestContainer[index].style.display='none';
-      refreshLoginSession(currentUser); */
+      
       })
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  const switchNotif=(type)=>{
+    const friendReqMain= document.querySelector('.friendReq-main');
+    const likeAndCommentMain = document.querySelector('.likeAndComment-main');
+    const friendReqHeader= document.querySelector('#friendReq-select');
+    const likeAndCommentHeader= document.querySelector('#likeAndComment-select');
+    if(type==='friend'){
+      friendReqMain.style.display='block';
+      likeAndCommentMain.style.display='none';
+      friendReqHeader.style.color='var(--purple)';
+      likeAndCommentHeader.style.color='var(--bg2)';
+
+    } else if(type==='likeAndComment'){
+      friendReqMain.style.display='none';
+      likeAndCommentMain.style.display='block';
+      friendReqHeader.style.color='var(--bg2)';
+      likeAndCommentHeader.style.color='var(--purple)';
+    }
+  }
+
+  const getUserInfoNotification=(authorId)=>{
+    fetch(`http://localhost:5000/users/${authorId}`)
+    .then((response) => response.json())
+    .then((data) =>{ 
+      console.log(data.email);
+      return data.email;
+    });
   }
 
   useEffect(()=>{
@@ -80,7 +103,12 @@ export function NotificationPage() {
       <Dashboard  dashIndex={4} />
       <div className='main'>
         <div className='notif-page'>
-        <div className='friendReq-head'>Friend Request</div>
+        <div className='friendReq-head'>
+          <div id='friendReq-select' onClick={()=>switchNotif('friend')}>
+            Friend Request</div>
+          <div id='likeAndComment-select' onClick={()=> switchNotif('likeAndComment')}>
+            Like and Comment</div>
+        </div>
         <div className='friendReq-main'>
 
         {currentUser.friendRequest.length > 0 ? (currentUser.friendRequest).map((data,index)=>{
@@ -106,6 +134,19 @@ export function NotificationPage() {
             )
         }): <div  className='friendReq-cont'> You have no friend request</div>}
         </div>
+        <div className='likeAndComment-main'>
+           {(currentUser.postNotification).length > 0 ? (currentUser.postNotification).map((item,index)=>{
+              return (
+              <div>
+                <div>{getUserInfoNotification(item.byUser)}</div>
+                <div>{item.action} your post
+                {item.byUser}</div>
+              </div>
+              )
+           }):
+           <div  className='friendReq-cont'> Notification clear</div>
+           }
+           </div>
         </div>
       </div>
       
