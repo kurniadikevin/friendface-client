ba4e55afee15428e3947bcf50c9e8e9e464323ad
kurniadikevin <kurniadikevin@gmail.com>
2023-03-30 20:21:20 +0700

    render group chat on chatroomlist

--- src/pages/Message/index.js
+++ src/pages/Message/index.js
@@ -21,8 +21,14 @@ export function MessagePage() {
       const listChat =data[0].chatRoomList;
       console.log(data[0].chatRoomList);
       setChatRoomList(data[0].chatRoomList);
+
       const filterListChat = listChat.map((i)=>{
-        return i.membersId[1];
+        if((i.membersId).length > 2){
+        return i.membersId;
+        } else{
+          return i.membersId[1];
+        }
+        
       });
       console.log(filterListChat);
       getUserInfoOnChatRoom(filterListChat);
@@ -35,16 +41,38 @@ export function MessagePage() {
 
   const getUserInfoOnChatRoom=(sourceData)=>{
      const data = Promise.all(
-        sourceData.map(async (i) => await (await fetch(`http://localhost:5000/users/simplified/${i}`, {
+        sourceData.map(async (i) => {
+          if(typeof i === 'object'){
+             const resultArr=[];
+              i.forEach(async(j)=>{
+              const data= (await fetch(`http://localhost:5000/users/simplified/${j}`, {
+              method: "GET",
+              })).json();
+              data.then((value)=>{
+                resultArr.push(value[0]); 
+              })
+            })
+          return resultArr;
+          } else{
+            const result= (await fetch(`http://localhost:5000/users/simplified/${i}`, {
             method: "GET",
-        })).json())
+          })).json()
+          return result;
+        }
+      }
+        )
     ).then((value)=>{
-      console.log(value)
-      const emailValue= value.map((i)=>{
-        return i[0].email;
+      console.log(value);
+      const memberValue= value.map((i)=>{
+        if(i.length === 1){
+          return i[0];
+        } else {
+          return i;
+        }
+       
       })
-      console.log(emailValue);
-      setChatRoomUserInfoList(emailValue);
+      console.log(memberValue);
+      setChatRoomUserInfoList(memberValue);
   })
   }
   
@@ -60,24 +88,23 @@ export function MessagePage() {
       <Dashboard  dashIndex={3} />
       <div className='main' id='message-main'>
         <div className='message-userChat-container'>
-        {chatRoomList.map((item,index)=>{
+          {chatRoomUserInfoList.map((item,index)=>{
 
           return(
-            <div>Chat room id : {item.membersId[1]}
-         </div>
+            <div className='chatRoom-container'>
+              <div className='chatRoom-member'>
+                { item.length > 1 ?
+                 <div>
+                  {item.map((i)=>{
+                    return(<div>Group {i.username}  {i.email}</div>)
+                  })}
+                </div> 
+                : <div>{item.username}  {item.email}</div>}
+                </div>
+              <div className='chatRoom-lastContent'>chat last content</div>
+          </div>
           )
-        })}
-
-{chatRoomUserInfoList.map((item,index)=>{
-
-return(
-  <div>Chat email: {item}
-</div>
-)
-})}
-
-
-
+          })}
         </div>
         <div className='message-content-container'></div>
       </div>
