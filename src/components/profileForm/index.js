01a456e78119a3b89967c76a68f37167d54aed38
kurniadikevin <kurniadikevin@gmail.com>
2023-04-14 18:21:31 +0700

    add reload for fix bug when update username

--- src/components/profileForm/index.js
+++ src/components/profileForm/index.js
@@ -38,6 +38,7 @@ const ProfileForm = (props)=>{
             refreshLoginSession(currentUser);
             const usernameDisplay=document.querySelector('#user-username');
             usernameDisplay.textContent=data;
+            setTimeout(()=> window.location.reload(false),1000);
            })
            .catch(function (error) {
              console.log(error);
